f8eafbd8a0dd2fc89a9c7910fe7a65f8562154e3
Aury Rodriguez <arm010286@hotmail.com>
2017-09-14 01:13:06 -0400

    add colunms transaction

--- db/migrate/20170914042654_create_transactions.rb
+++ db/migrate/20170914042654_create_transactions.rb
@@ -2,6 +2,12 @@ class CreateTransactions < ActiveRecord::Migration[5.1]
   def change
     create_table :transactions do |t|
 
+      t.text :note
+      t.string :date ,:limit => 10
+      t.decimal :amount,  scale: 2
+      t.string :receipt  ,:limit => 20
+      t.belongs_to :category, index: true
+
       t.timestamps
     end
   end
