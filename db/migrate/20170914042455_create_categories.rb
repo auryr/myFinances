a99f633fc4cd3ae73fbcd50d72a6f500bfbe76ac
Aury Rodriguez <arm010286@hotmail.com>
2017-09-14 01:13:39 -0400

    change category

--- db/migrate/20170914042455_create_categories.rb
+++ db/migrate/20170914042455_create_categories.rb
@@ -5,6 +5,8 @@ class CreateCategories < ActiveRecord::Migration[5.1]
       t.string :name
       t.string :description
       t.string :operation
+      t.boolean :included
+      t.belongs_to :user, index: true
 
       t.timestamps
     end
