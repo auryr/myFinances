4efbf6bba14e135f7efc9ea9c4752b372dbc2656
Aury Rodriguez <arm010286@hotmail.com>
2017-09-14 01:12:41 -0400

    add colunms budget

--- db/migrate/20170914042559_create_budgets.rb
+++ db/migrate/20170914042559_create_budgets.rb
@@ -2,6 +2,13 @@ class CreateBudgets < ActiveRecord::Migration[5.1]
   def change
     create_table :budgets do |t|
 
+      t.string :name
+      t.string :description
+      t.string :initdate ,:limit => 10
+      t.string :enddate  ,:limit => 10
+      t.decimal :amount,  scale: 2
+      t.belongs_to :user, index: true
+
       t.timestamps
     end
   end
